8e30bac24b0f3bbbaf15db178a53d3bcfdbe0606
RaffaeleCaravetta <raffaelecaravetta13@gmail.com>
2023-10-03 15:19:49 +0200

    Terzo esercizio

--- src/ExerciseN3/ExerciseN3.java
+++ src/ExerciseN3/ExerciseN3.java
@@ -1,10 +1,30 @@
 package ExerciseN3;
 
+import java.util.Scanner;
+
 public class ExerciseN3 {
 
     public static void main(String[] args) {
 
         System.out.println("Inserisci una stringa!");
 
+        Scanner scanner = new Scanner(System.in);
+
+        String string = scanner.nextLine();
+        String splittedString ="";
+        while(!string.equals(":q")) {
+                for (int i = 0; i < string.length(); i++) {
+                    if(i<string.length()-1){
+                        splittedString +=  string.charAt(i)+(",") ;
+                    }else{
+                        splittedString +=  string.charAt(i) ;
+                    }
+
+                }
+                System.out.println(splittedString);
+            string = scanner.nextLine();
+            splittedString = "";
+        }
+scanner.close();
     }
 }
